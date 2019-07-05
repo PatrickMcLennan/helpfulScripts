8e2d98e67dabbf8948c169c4e7058e38f6406d31
Patrick <patrick.a.mclennan@gmail.com>
2019-07-04 21:52:59 -0400

    first commit

--- episodeCounter/.episodeCounter.js
+++ episodeCounter/.episodeCounter.js
@@ -7,31 +7,33 @@ const NEW_EXT = `.${process.argv[2]}`;
 const OLD_EXT = `.${process.argv[3]}`;
 let COUNTER = 1;
 
-fs.readdir(here, (err, files) => {
-    if (err) {
-        console.error(err);
-    } else if (ARG_COUNT === 2) {
-        return console.log(`
-        ---------------------------------------------------
-        You must pass at least 1 argument into this script.
-        ---------------------------------------------------
-        `);
-    } else {
-        files.forEach(file => {
-            const fileExt = path.extname(file);
-            const newName = `Episode ${COUNTER}${NEW_EXT}`;
+if (ARG_COUNT <= 2) {
+    console.log(`
+    ---------------------------------------------------
+    You must pass at least 1 argument into this script.
+    ---------------------------------------------------
+    `);
+} else {
+    fs.readdir(here, (err, files) => {
+        if (err) {
+            console.error(err);
+        } else {
+            files.forEach(file => {
+                const fileExt = path.extname(file);
+                const newName = `Episode ${COUNTER}${NEW_EXT}`;
 
-            if (
-                (ARG_COUNT === 3 && fileExt === NEW_EXT) ||
-                (ARG_COUNT === 4 && fileExt === OLD_EXT)
-            ) {
-                fs.rename(file, newName, err =>
-                    err
-                        ? console.error(err)
-                        : console.log`Renaming ${file} to ${newName}`
-                );
-                COUNTER += 1;
-            }
-        });
-    }
-});
+                if (
+                    (ARG_COUNT === 3 && fileExt === NEW_EXT) ||
+                    (ARG_COUNT === 4 && fileExt === OLD_EXT)
+                ) {
+                    fs.rename(file, newName, err =>
+                        err
+                            ? console.error(err)
+                            : console.log`Renaming ${file} to ${newName}`
+                    );
+                    COUNTER += 1;
+                }
+            });
+        }
+    });
+}
