2bf32497f1139ddaff9ddd1926a23064ba28c69f
Patrick McLennan <patrick.a.mclennan@gmail.com>
2019-08-25 14:05:31 -0400

    blah

--- cronJob/cron.js
+++ cronJob/cron.js
@@ -1,30 +1,22 @@
 const fs = require('fs')
 const path = require('path')
 const https = require('https')
+require('dotenv').config()
 
-const here = path.resolve();
-
-const now = new Date();
+const HERE = path.resolve()
 
 const moveThatBus = () => https.get('https://jsonplaceholder.typicode.com/posts', res => {
     let data = '';
 
-    res.on('data', chunk => data += chunk);
+    res.on('data', chunk => data += chunk)
 
-    res.on('end', () => {
-        const timeStamp = () => `${now.getHours() > 12 ? now.getHours() - 12 : now.getHours()}:${now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes()}`
-        const dateAdder = string => JSON.stringify(JSON.parse(string).concat([{dateStamp: `This was scraped at ${timeStamp()}`}]))
-        fs.writeFile(`${here}/yolo.txt`, dateAdder(data), err =>
+    res.on('end', () => 
+        fs.writeFile(`${HERE}/yolo.txt`, JSON.stringify([...JSON.parse(data), { timeStamp: 'hello' }]), err =>
             err
                 ? console.error(err)
-                : console.log(`Successfully ran at ${timeStamp()}`)
+                : console.log('nailed it babaaayyy')
         )
-        return setTimeout(() => moveThatBus(), 30000)
-    }
     )
-}).on('error', err => {
-    console.error(`ERROR: ${err}`);
-    return process.exit(1)
-})
+}).on('error', err => console.error(err));
 
 moveThatBus()
